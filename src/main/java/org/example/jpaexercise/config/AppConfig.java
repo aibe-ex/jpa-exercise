2b7e93053e6aac428fa57c7c125c026fa905971e
dochmai382 <dochmai382@gmail.com>
2025-04-02 15:28:33 +0900

    config

---
+++ src/main/java/org/example/jpaexercise/config/AppConfig.java
@@ -0,0 +1,10 @@
+package org.example.jpaexercise.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.jpaexercise")
+public class AppConfig {
+
+}
